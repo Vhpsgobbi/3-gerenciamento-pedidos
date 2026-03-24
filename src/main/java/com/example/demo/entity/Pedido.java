9387afc39967cc6526c0e2a06cbb27599a9d96dc
VictorSgobbi <victorhugosgobbi@gmail.com>
2026-03-24 00:00:38 -0300

    Criação da entidade Pedido

---
+++ src/main/java/com/example/demo/entity/Pedido.java
@@ -0,0 +1,59 @@
+package com.example.demo.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+
+@Entity(name = "Pedido")
+@Table(name = "tb_pedido")
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "dt_pedido")
+    private LocalDateTime data;
+
+    @Column(name = "vl_total")
+    private BigDecimal valorTotal;
+
+    @Column(name = "ds_status")
+    private String status;
+
+    public Pedido() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
