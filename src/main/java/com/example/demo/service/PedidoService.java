7b61cb4795341e674b15af3ee4d2851b35217900
VictorSgobbi <victorhugosgobbi@gmail.com>
2026-03-24 00:01:12 -0300

    Criação do service PedidoService

---
+++ src/main/java/com/example/demo/service/PedidoService.java
@@ -0,0 +1,32 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Pedido;
+import com.example.demo.repository.PedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PedidoService {
+
+    @Autowired
+    private PedidoRepository pedidoRepository;
+
+    public List<Pedido> findAll() {
+        return pedidoRepository.findAll();
+    }
+
+    public Pedido criar(Pedido pedido) {
+        return pedidoRepository.save(pedido);
+    }
+
+    public Optional<Pedido> buscarPorId(Long id) {
+        return pedidoRepository.findById(id);
+    }
+
+    public void deletar(Long id) {
+        pedidoRepository.deleteById(id);
+    }
+}
